6837c8d0c6816728f2ebd92878a51d1515d3ec37
matt1light <matt1light@gmail.com>
2017-09-20 15:11:13 -0400

    Add free agent scraper for NFL.com Leagues

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -43,7 +43,12 @@ end
 class FantasyProsScraper < RankingsScraper
   def initialize
     super
-    @url = 'https://www.fantasypros.com/2017/09/fantasy-football-trade-values-week-2/'
+    make_url(3)
+  end
+
+  def make_url(week)
+    #TODO Make it handle other months!!!
+    @url = 'https://www.fantasypros.com/2017/09/fantasy-football-trade-values-week-' + week.to_s
   end
   
   def scrape_player_values
@@ -69,8 +74,23 @@ class LeagueScraper < Scraper
     @league = league
     @team = team
     @players = []
+    @league_size
   end
 
+  def get_players
+    i = 1
+    @league_size.times do
+      scrape_team(i)
+    end
+		#TODO: add function to determine week and year and unhardcode it
+    scrape_waivers(3, 2017)
+  end
+	
+  def scrape_waivers(week, year)
+		make_urls(week, year).each do |url|
+			scrape_waiver_page(url)
+    end
+  end
 end
 
 class NFLScraper < LeagueScraper
@@ -98,7 +118,7 @@ class NFLScraper < LeagueScraper
       # if the player is a defense then it has no team value
       if position_team == 'DEF'
         position = position_team
-        team = nil
+        team_name = nil
       else
         # if it is not a defense split the text into position and team
         position = position_team.slice(0..(position_team.index(' ') - 1))
@@ -108,6 +128,51 @@ class NFLScraper < LeagueScraper
       @players << {name: name, position: position, team: number, nflteam: team_name}
     end
   end
+
+
+	def scrape_waiver_page(url)
+		page = open_page(url)
+    rows = page.css('td').select{|c| c['class'].include?('playerNameAndInfo')}
+		rows.each do |playerNameAndInfo|
+			name = playerNameAndInfo.css('a').select{|c| c['class'].include?('playerName')}[0]&.text
+			position_team = playerNameAndInfo.css('em').text
+			if !position_team.include?('-')
+				position = position_team
+				team_name = nil
+			else
+				# if it is not a defense split the text into position and team
+				position = position_team.slice(0..(position_team.index(' ') - 1))
+				team_name = position_team.slice((position_team.index('-') + 2)..-1)
+			end
+			# add a hash with all of these values to the players array
+			@players << {name: name, position: position, team: 0, nflteam: team_name}
+		end
+	end
+
+	def make_urls(week, year)
+		urls = []
+		# each of these numbers represents a position
+		# 1: qb
+		# 2: WR
+		# 3: RB
+		# 4: TE
+		# 5: WR/RB
+		# 7: Kicker
+    # 8: Defense
+		[1, 2, 3, 4, 7, 8].each do |position_number|
+			url = 'http://fantasy.nfl.com/league/' +
+						@league + 
+						'/players?_selectedColumnSortOrder_=desc&playerStatus=available&position=' +
+						position_number.to_s + 
+						'&sort=percentOwned&statCategory=research&statSeason=' +
+            year.to_s + 
+						'&statWeek=' +
+						week.to_s
+			urls << url
+		end
+		urls
+	end
+	# URLS are from NFL.com free agents list ordered by % ownership
 end
 
 class YahooScraper < LeagueScraper
