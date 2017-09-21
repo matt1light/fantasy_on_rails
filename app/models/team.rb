fbd07044eeaa73b470f5f6b0e19176094406db3a
matt1light <matt1light@gmail.com>
2017-09-21 14:38:51 -0400

    add make_players to team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,55 +1,72 @@
 require 'open-uri'
+
+
 class Team < ActiveRecord::Base
     belongs_to :league, inverse_of: :team
     has_and_belongs_to_many :trade, inverse_of: :team
     has_many :player, inverse_of: :team
 
-
-    def make_url
-        if self.league.site == 'NFL'
-            puts 'http://canada.fantasy.nfl.com/league/' +
-                 self.league.number.to_s +
-                 '/team/' +
-                 self.number.to_s
-            return 'http://canada.fantasy.nfl.com/league/' +
-                    self.league.number.to_s +
-                    '/team/' +
-                    self.number.to_s
-        elsif self.league.site == 'ESPN'
-            return 'http://games.espn.com/ffl/clubhouse?leagueId=' +
-                    self.league.number.to_s +
-                    '8&teamId=' +
-                    self.number.to_s
-        elsif self.league.site == 'Yahoo'
-            return 'https://football.fantasysports.yahoo.com/f1/' +
-            self.league.number.to_s +
-            '/' +
-            self.number.to_s
+    def make_players(scraped_players, scraped_values)
+      scraped_players.each do |play|
+        name = play[:name]
+        position = play[:position]
+        play_val = scraped_values.select{|p| p[:name] == name} 
+        if play_val.length == 1
+          value = play_val.first[:value]
+          Player.create( name: name,
+                         team: self,
+                         position: position,
+                         value: value)
+        else
+          puts 'There is more than one player with that name and position'
         end
+      end
     end
+    # def make_url
+    #     if self.league.site == 'NFL'
+    #         puts 'http://canada.fantasy.nfl.com/league/' +
+    #              self.league.number.to_s +
+    #              '/team/' +
+    #              self.number.to_s
+    #         return 'http://canada.fantasy.nfl.com/league/' +
+    #                 self.league.number.to_s +
+    #                 '/team/' +
+    #                 self.number.to_s
+    #     elsif self.league.site == 'ESPN'
+    #         return 'http://games.espn.com/ffl/clubhouse?leagueId=' +
+    #                 self.league.number.to_s +
+    #                 '8&teamId=' +
+    #                 self.number.to_s
+    #     elsif self.league.site == 'Yahoo'
+    #         return 'https://football.fantasysports.yahoo.com/f1/' +
+    #         self.league.number.to_s +
+    #         '/' +
+    #         self.number.to_s
+    #     end
+    # end
 
-    def get_team
-        doc = Nokogiri::HTML(open(self.make_url))
-        if self.league.site == 'NFL'
-            playerNames = doc.xpath("//a[contains(@class, 'playerName')]")
-            for name in playerNames do
-                position = name.next_element().text[0..1]
-                Player.create(name: name.text, position: position, team: self)
-            end
-        elsif self.league.site == 'ESPN'
-            playerNames = doc.xpath("//*(@class='flexpop')")
-            for name in playerNames do
-                position = name.next().text[-3..-1]
-                Player.create(name: name, position: position, team: self)
-            end
-        elsif self.league.site == 'yahoo'
-            playerNames = doc.xpath("//*(@class='Nowrap name F-link')")
-            for name in playerNames do
-                position = name.next_element().text[-3..-1]
-                Player.create(name: name, position: position, team: self)
-            end
-        end
-    end
+    # def get_team
+    #     doc = Nokogiri::HTML(open(self.make_url))
+    #     if self.league.site == 'NFL'
+    #         playerNames = doc.xpath("//a[contains(@class, 'playerName')]")
+    #         for name in playerNames do
+    #             position = name.next_element().text[0..1]
+    #             Player.create(name: name.text, position: position, team: self)
+    #         end
+    #     elsif self.league.site == 'ESPN'
+    #         playerNames = doc.xpath("//*(@class='flexpop')")
+    #         for name in playerNames do
+    #             position = name.next().text[-3..-1]
+    #             Player.create(name: name, position: position, team: self)
+    #         end
+    #     elsif self.league.site == 'yahoo'
+    #         playerNames = doc.xpath("//*(@class='Nowrap name F-link')")
+    #         for name in playerNames do
+    #             position = name.next_element().text[-3..-1]
+    #             Player.create(name: name, position: position, team: self)
+    #         end
+    #     end
+    # end
 ############### TODO fix this so it works for fucking leveon bell
     def get_values
         doc = Nokogiri::HTML(open('http://www.cbssports.com/fantasy/football/news/fantasy-football-week-11-rankings-trade-values-chart/'))
