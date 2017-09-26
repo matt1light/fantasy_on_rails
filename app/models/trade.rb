0432acc5cbdc1ebd850bcc9387374cc2e033afdd
matt1light <matt1light@gmail.com>
2017-09-25 22:08:58 -0400

    Add one for one trades

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,4 +2,21 @@ class Trade < ActiveRecord::Base
     belongs_to :league, inverse_of: :trade
     has_and_belongs_to_many :team, inverse_of: :trade
     has_and_belongs_to_many :player, inverse_of: :trade
+
+
+    def get_players
+      self.player
+    end
+
+    def total_value
+      self.value_1 + self.value_2
+    end
+
+    def value_1
+      self.my_value
+    end
+
+    def value_2
+      self.other_value
+    end
 end
