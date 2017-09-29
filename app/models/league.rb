4fc29bd3b46087e6b63f02ccc6610c0d700010b3
matt1light <matt1light@gmail.com>
2017-09-29 01:43:11 -0400

    trade sheet and team migration

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,6 +2,7 @@ require 'scraper'
 class League < ActiveRecord::Base
     has_many :team, inverse_of: :league, dependent: :destroy
     has_many :players, dependent: :destroy
+    has_many :trade_sheets, inverse_of: :league, dependent: :destroy
 
     def scrape_league
       if self.site == 'NFL'
@@ -15,7 +16,10 @@ class League < ActiveRecord::Base
       league_scraper.scrape_teams_info
       league_scraper.scrape_players
 
-      make_teams(league_scraper.get_league_size, league_scraper.get_players, league_scraper.get_teams, rankings_scraper.get_player_list)
+      make_teams(league_scraper.get_league_size,
+                 league_scraper.get_players,
+                 league_scraper.get_teams,
+                 rankings_scraper.get_player_list)
     end
 
     def make_teams(size, scraped_players, scraped_teams, scraped_values)
