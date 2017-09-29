5cf1c6870b1c633c8610974f33eb9ee303222b74
matt1light <matt1light@gmail.com>
2017-09-29 01:44:20 -0400

    Add tradesheets to models

--- test/models/trade_sheet_test.rb
+++ test/models/trade_sheet_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TradeSheetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+  end
+
+  test 'test the new trade_sheet one for one trade' do
+    @league = League.new()
+    @league.update(number: 4430415, site: 'NFL')
+    @league.scrape_league
+    @ts = TradeSheet.new(league: @league)
+    @ts.update(team: [@league.team[1], @league.team[2]])
+    @ts.one_for_one_trades
+    binding.pry
+  end
 end
